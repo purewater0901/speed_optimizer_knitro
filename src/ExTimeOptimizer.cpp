153e8950b4e32811b1343f125760bc9bb4e66f14
yutaka <purewater0901@gmail.com>
2019-10-10 14:36:48 +0900

    add constraints

--- src/ExTimeOptimizer.cpp
+++ src/ExTimeOptimizer.cpp
@@ -24,8 +24,8 @@ cv::Point2i cv_offset2(float x, float y)
 
 int main()
 {
-    double predictDistance = 50.0;
-    double ds = 0.5;
+    double predictDistance = 20.0;
+    double ds = 0.2;
     std::string waypointFilename = "../data/saved_waypoints.csv";
     std::string filename = "../position_result.csv";
 
@@ -34,43 +34,51 @@ int main()
     ReferencePath referencePath(waypoints.x_, waypoints.y_, ds);
     int N = referencePath.x_.size();
 
+    // another information
+    double m = 1500;
+    double V0 = 1.0;
+    double a0 = 0.0;
+    double mu = 0.8;
+
     // speed restriction and acceleration restriction
     std::vector<double> Vr;
+    std::vector<double> Vd;
     std::vector<double> Arlon;  //acceleration longitudinal restriction
     std::vector<double> Arlat;  //acceleration lateral restriction
     std::vector<double> Aclon;  //comfort longitudinal acceleration restriction
     std::vector<double> Aclat;  //comfort lateral acceleration restriction
     Vr.resize(N);
+    Vd.resize(N);
     Arlon.resize(N);
     Arlat.resize(N);
     Aclon.resize(N);
     Aclat.resize(N);
 
-    for(size_t i=1; i<Vr.size()/2; ++i)
+    for(size_t i=0; i<Vr.size()/2; ++i)
+    {
         Vr[i] = 2.0;
+        Vd[i] = 1.5;
+    }
 
     for(size_t i=Vr.size()/2; i<Vr.size(); ++i)
+    {
         Vr[i] = 4.0;
-
-    Vr[0] = 1.0;
+        Vd[i] = 3.5;
+    }
 
     for(size_t i=0; i<N; ++i)
     {
-        Arlon[i] = 1.2;
-        Arlat[i] = 0.2;
-        Aclon[i] = 0.8;
-        Aclat[i] = 0.1;
+        Arlon[i] = 0.5*mu*9.83;
+        Arlat[i] = 0.5*mu*9.83;
+        Aclon[i] = 0.4*mu*9.83;
+        Aclat[i] = 0.4*mu*9.83;
     }
 
-    // another information
-    double m = 500;
-    double a0 = 0.0;
-    double mu = 0.8;
 
-    std::array<double, 5> weight = {1.0, 0.025, 0, 0.02, 0.02};
+    std::array<double, 5> weight = {1.0, 5.0, 0.0, 10, 10};
 
     // Create a problem instance.
-    TimeOptimizer instance(N, Vr, Arlon, Arlat, Aclon, Aclat, weight, referencePath, m, ds, a0, mu);
+    TimeOptimizer instance(N, Vr, Vd, Arlon, Arlat, Aclon, Aclat, weight, referencePath, m, ds, V0, a0, mu);
 
     // Create a solver
     knitro::KTRSolver solver(&instance, KTR_GRADOPT_FORWARD, KTR_HESSOPT_BFGS);
@@ -84,11 +92,17 @@ int main()
     /*  Write to file*/
     std::ofstream writing_file;
     writing_file.open(filename, std::ios::out);
-    writing_file << "position" << "," << "reference_speed" <<  "," << "result_speed" <<  ","
-                 << "acceleration" << "," << "longitudinal_slack_variable"<<std::endl;
+    writing_file << "position" << ","
+                 << "reference_speed" <<  "," << "desired_speed" << ","
+                 << "result_speed" <<  ","
+                 << "longitudinal acceleration" << "," << "lateral acceleration" << ","
+                 << "longitudinal_slack_variable" << "," << "lateral_slack_variable" <<std::endl;
     for(int i=0; i<N; ++i)
-        writing_file << i*ds<< "," << Vr[i] << "," << std::sqrt(point[i]) << "," << point[i+N] << ","
-                     << point[i+2*N] << std::endl;
+        writing_file << i*ds<< ","
+                     << Vr[i] << "," << Vd[i] << ","
+                     << std::sqrt(point[i]) << ","
+                     << point[i+N] << "," << point[i+5*N]/m << ","
+                     << point[i+2*N] << "," << point[i+3*N]<< std::endl;
     writing_file.close();
 
     cv::namedWindow("optimized_speed", cv::WINDOW_NORMAL);
@@ -100,6 +114,12 @@ int main()
                 bg,
                 cv_offset(i*ds, -Vr[i], bg.cols, bg.rows),
                 cv_offset((i+1)*ds, -Vr[i+1], bg.cols, bg.rows),
+                cv::Scalar(0, 0, 255),
+                10);
+        cv::line(
+                bg,
+                cv_offset(i*ds, -Vd[i], bg.cols, bg.rows),
+                cv_offset((i+1)*ds, -Vd[i+1], bg.cols, bg.rows),
                 cv::Scalar(0, 0, 255),
                 10);
     }
--- src/ExTimeOptimizer.h
+++ src/ExTimeOptimizer.h
@@ -15,6 +15,7 @@ class TimeOptimizer : public knitro::KTRProblem
 public:
     TimeOptimizer(const int N,
                   const std::vector<double>& Vr,
+                  const std::vector<double>& Vd,
                   const std::vector<double>& Arlon,
                   const std::vector<double>& Arlat,
                   const std::vector<double>& Aclon,
@@ -23,11 +24,12 @@ public:
                   const ReferencePath& referencePath,
                   const double m,
                   const double ds,
+                  const double V0,
                   const double a0,
                   const double mu)
-                  : KTRProblem(6*N, 5*N), N_(N), weight_(weight), referencePath_(referencePath),
-                    Vr_(Vr), Arlon_(Arlon), Arlat_(Arlat), Aclon_(Aclon), Aclat_(Aclat),
-                    m_(m), epsilon_(1.0e-6), ds_(ds), a0_(a0), mu_(mu), g_(9.80665)
+                  : KTRProblem(6*N, 5*N+2*(N-3)), N_(N), weight_(weight), referencePath_(referencePath),
+                    Vr_(Vr), Vd_(Vd), Arlon_(Arlon), Arlat_(Arlat), Aclon_(Aclon), Aclat_(Aclat),
+                    m_(m), epsilon_(1.0e-6), ds_(ds), V0_(V0), a0_(a0), mu_(mu), g_(9.80665)
     {
         /*
          * @ variables
@@ -58,7 +60,7 @@ public:
         for(int i=0; i<N_; ++i)
         {
             c[i+N_]   = Aclon_[i] + x[i+2*N_] - std::fabs(x[i+N_]);      //longitudinal
-            c[i+2*N_] = Aclat_[i] + x[1+3*N_] - std::fabs(x[i+5*N_]/m_); //lateral
+            c[i+2*N_] = Aclat_[i] + x[i+3*N_] - std::fabs(x[i+5*N_]/m_); //lateral
         }
 
         /* constraint3 friction circle */
@@ -66,6 +68,20 @@ public:
         {
             c[i+3*N_] = mu_*m_*g_ - std::sqrt(x[i+4*N_]*x[i+4*N_] + x[i+5*N_]*x[i+5*N_]);
             c[i+4*N_] = m_*Arlon_[i] - x[i+4*N_];
+        }
+
+        /* constraint4 dynamics constraints*/
+        for(int i=2; i<N_-1; ++i)
+        {
+            double rx1 = (referencePath_.x_[i+1]-referencePath_.x_[i])/ds_;
+            double rx2 = (referencePath_.x_[i-2]-referencePath_.x_[i-1]-referencePath_.x_[i]+referencePath_.x_[i+1])/(2*ds_*ds_);
+            double leftx = x[i+4*N_]*cos(referencePath_.yaw_[i]) - x[i+5*N_]*sin(referencePath_.yaw_[i]);
+            c[(i-2)+5*N_] = leftx - (rx2 * x[i] + rx1 * x[i+N_]);
+
+            double ry1 = (referencePath_.y_[i+1]-referencePath_.y_[i])/ds_;
+            double ry2 = (referencePath_.y_[i-2]-referencePath_.y_[i-1]-referencePath_.y_[i]+referencePath_.y_[i+1])/(2*ds_*ds_);
+            double lefty = x[i+4*N_]*sin(referencePath_.yaw_[i]) + x[i+5*N_]*cos(referencePath_.yaw_[i]);
+            c[(i-2)+6*N_-3] = lefty - (ry2 * x[i] + ry1 * x[i+N_]);
         }
 
         double Jt = 0.0;
@@ -78,11 +94,11 @@ public:
         {
             Jt += (2*ds_)/(std::sqrt(x[i])+std::sqrt(x[i+1])+epsilon_);
             Js += std::pow((x[i+N_+1] - x[i+N_])/ds_, 2);
-            Jv += std::pow(x[i] - Vr_[i]*Vr_[i], 2)*ds_;
+            Jv += std::pow(x[i] - Vd_[i]*Vd_[i], 2)*ds_;
             LonSlack += std::fabs(x[i+2*N_]);
             LatSlack += std::fabs(x[i+3*N_]);
         }
-        Jv += std::pow(x[N_-1] - Vr_[N_-1]*Vr_[N_-1], 2)*ds_;
+        Jv += std::pow(x[N_-1] - Vd_[N_-1]*Vd_[N_-1], 2)*ds_;
         LonSlack += std::fabs(x[3*N_-1]);
         LatSlack += std::fabs(x[4*N_-1]);
 
@@ -104,8 +120,8 @@ private:
         {
             if (i == 0)
             {
-                setVarLoBnds(i, Vr_[i] * Vr_[i]);
-                setVarUpBnds(i, Vr_[i] * Vr_[i]);
+                setVarLoBnds(i, V0_ * V0_);
+                setVarUpBnds(i, V0_ * V0_);
 
             } else
             {
@@ -158,10 +174,21 @@ private:
             setConLoBnds(i+4*N_, 0.0);
             setConUpBnds(i+4*N_, KTR_INFBOUND);
         }
+
+        for(int i=2; i<N_-1; ++i)
+        {
+            setConTypes((i-2)+5*N_, knitro::KTREnums::ConstraintType::ConGeneral);
+            setConLoBnds((i-2)+5*N_, 0.0);
+            setConUpBnds((i-2)+5*N_, 0.0);
+            setConTypes((i-2)+6*N_-3, knitro::KTREnums::ConstraintType::ConGeneral);
+            setConLoBnds((i-2)+6*N_-3, 0.0);
+            setConUpBnds((i-2)+6*N_-3, 0.0);
+        }
     }
 
     std::array<double,5> weight_;
     std::vector<double> Vr_;
+    std::vector<double> Vd_;
     std::vector<double> Arlon_;
     std::vector<double> Arlat_;
     std::vector<double> Aclon_;
@@ -171,6 +198,7 @@ private:
     const double epsilon_;
     double m_;
     double ds_;
+    double V0_;
     double a0_;
     int N_;
     double mu_;
